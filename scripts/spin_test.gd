7d0bcec7e80194928e58aaab4de463a563de8934
jaaaaaaaaanaaa <janaamrmo123@icloud.com>
2026-08-17 16:44:43 +0300

    Changed var name

--- scripts/spin_test.gd
+++ scripts/spin_test.gd
@@ -2,7 +2,7 @@ extends Node3D
 class_name SpinTest
 
 @export var pedestal: Node3D
-@export var rotation_speed: float = 2
+@export var rotation_speeds: float = 2
 
 func _process(delta: float) -> void:
-	pedestal.rotate_y(rotation_speed * delta)
+	pedestal.rotate_y(rotation_speeds * delta)
